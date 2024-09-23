206b58d532865ae1379c79d98b36cb2dd773ee73
bln51000 <shanegibson1221@gmail.com>
2024-09-22 22:17:24 -0400

    HW3

---
+++ Assets/FloatingScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingScript : MonoBehaviour
+{
+    MeshRenderer meshRenderer;
+    new Rigidbody rigidbody;
+
+    float timeToWait = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Capture references at the start of the game
+        meshRenderer = GetComponent<MeshRenderer>();
+        rigidbody = GetComponent<Rigidbody>();
+
+        meshRenderer.enabled = false;
+        rigidbody.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > timeToWait)
+        {
+            meshRenderer.enabled = true;
+            rigidbody.useGravity = true;
+        }
+    }
+}
